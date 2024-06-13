6081d5cd464fd4c87685ba9e21e6ff17b0c41463
bycold1 <114782495+bycold1@users.noreply.github.com>
2024-06-12 20:28:13 -0400

    pushing the game

---
+++ src/SnakeGame.java
@@ -0,0 +1,24 @@
+import javax.swing.JFrame;
+
+public class SnakeGame extends JFrame {
+
+    public SnakeGame() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new Board());
+        setResizable(false);
+        pack();
+        setTitle("Snake Game");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            JFrame ex = new SnakeGame();
+            ex.setVisible(true);
+        });
+    }
+}
